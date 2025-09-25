a2569a6d768bddd24644054d6a8af4c69e75485e
Rakesh Prasad <rakeshrocco28@gmail.com>
2025-09-25 18:06:55 +0530

    server url added

--- client/src/components/utilities/axiosInstance.js
+++ client/src/components/utilities/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const DB_URL = import.meta.env.VITE_DB_URI;
 
 const axiosInstance = axios.create({
-  baseURL: DB_URL,
+  baseURL: "https://whatsapp-server-taupe.vercel.app/",
   withCredentials: true,
   headers: {
     ContentType: "application/json",
